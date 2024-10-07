ee1696828290f5a948171e2e32f15d05edc2ca25
Cynthia De Meester <439122@student.fontys.nl>
2024-10-07 07:16:31 +0200

    transfer

--- Foeder-Frontend/src/App.jsx
+++ Foeder-Frontend/src/App.jsx
@@ -1,16 +1,38 @@
 import { useState } from 'react'
 import { useEffect } from 'react';
-import './App.css'
 
 export default function App() {
 
 
   return (
     <>
+      <Header />
       <RecipeList />
     </>
   )
 }
+
+function Header(){
+  return (
+    <div className="navbar bg-neutral flex">
+      <div className="flex-1">
+        <a className='btn btn-primary text-primary-content'>Foeder</a>
+      </div>
+      <div className="flex-auto">
+        <ul className="menu menu-horizontal">
+          <li><a className='text-neutral-content'>Recipes</a></li> 
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+// function ContentBox(){
+//   return (
+
+//   )
+// }
+
 
 function RecipeList() {
 
--- Foeder-Frontend/tailwind.config.js
+++ Foeder-Frontend/tailwind.config.js
@@ -1,12 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './app.jsx'
+     "./src/*.{html,js,jsx}",
   ],
-  theme: {
-    extend: {},
+  theme: [],
+  daisyui: {
+	  themes: [
+	  {
+		  mytheme: {
+			  "primary": "#DBD6CA",
+					  
+        "secondary": "#833232",
+              
+        "accent": "#57862E",
+              
+        "neutral": "#30321C",
+              
+        "base-100": "#FFFCF2",
+              
+        "info": "#F1F1CB",
+		  },
+	  }
+  ],
   },
-  plugins: [],
+  plugins: [
+	require('daisyui'),
+  ],
 }
 
