b574056fb908d8bef9124bf72ba0cf3748cf5511
Cynthia De Meester <439122@student.fontys.nl>
2024-10-14 13:20:42 +0200

    Start to google auth

---
+++ Foeder-Frontend/src/Authentication/GoogleAuth.jsx
@@ -0,0 +1,8 @@
+function GoogleAuth(){
+    return <div>
+        <script src="https://apis.google.com/js/platform.js" async defer></script>
+        <meta name="google-signin-client_id" content="367820938286-u0u38i2711n088ttaikh6s6td31pegjd.apps.googleusercontent.com"></meta>
+        <div class="g-signin2" data-onsuccess="onSignIn"></div>
+    </div>
+}
+
