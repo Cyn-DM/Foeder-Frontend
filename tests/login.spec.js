0b8f849ec2b63e5719aff6d11b6d58cfbe497b96
Cynthia De Meester <439122@student.fontys.nl>
2024-11-07 12:35:18 +0100

    added user agent to make headless accept

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -3,18 +3,26 @@ require('dotenv').config({ path: './secrets.env' });
 
 const foederLoginEmail = process.env.foederLoginEmail;
 const foederLoginPassword = process.env.foederLoginPass;
+const userAgent =
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36";
 
-test('login', async ({page}) => {
-    await page.goto('https://localhost:5173/');
-    const page1Promise = page.waitForEvent('popup');
-    await page.locator('iframe[title="Knop Inloggen met Google"]').contentFrame().locator('#container').click();
-    const page1 = await page1Promise;
-    await page1.getByLabel('Email or phone').click();
-    await page1.getByLabel('Email or phone').fill(foederLoginEmail);
-    await page1.getByRole('button', { name: 'Next' }).click();
-    await page1.getByLabel('Enter your password').click();
-    await page1.getByLabel('Enter your password').fill(foederLoginPassword);
-    await page1.getByRole('button', { name: 'Next' }).click();
-    await page1.getByRole('button', { name: /Doorgaan|Next/ }).click();
-    await expect(page.getByRole('paragraph')).toContainText('Welcome TestAccount');
-});
+test.describe("With user agent", () => {
+    test.use({userAgent});
+
+    test('login', async ({page}) => {
+
+        await page.goto('https://localhost:5173/');
+        const page1Promise = page.waitForEvent('popup');
+        await page.locator('iframe[title="Knop Inloggen met Google"]').contentFrame().locator('#container').click();
+        const page1 = await page1Promise;
+        await page1.getByLabel('Email or phone').click();
+        await page1.getByLabel('Email or phone').fill(foederLoginEmail);
+        await page1.getByRole('button', { name: 'Next' }).click();
+        await page1.getByLabel('Enter your password').click();
+        await page1.getByLabel('Enter your password').fill(foederLoginPassword);
+        await page1.getByRole('button', { name: 'Next' }).click();
+        await page1.getByRole('button', { name: /Doorgaan|Next/ }).click();
+        await expect(page.getByRole('paragraph')).toContainText('Welcome TestAccount');
+    });
+})
+
