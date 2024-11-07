2c123e299c749764bc3106ad84b87d4e85f88fa2
Cynthia De Meester <439122@student.fontys.nl>
2024-11-07 11:57:32 +0100

    Added playwright and wrote first draft of test

---
+++ tests/login.spec.js
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+require('dotenv').config({ path: './secrets.env' });
+
+const foederLoginEmail = process.env.foederLoginEmail;
+const foederLoginPassword = process.env.foederLoginPass;
+
+test('login', async ({}) => {
+    await page.goto('https://localhost:5173/');
+    const page1Promise = page.waitForEvent('popup');
+    await page.locator('iframe[title="Knop Inloggen met Google"]').contentFrame().getByLabel('Inloggen met Google').click();
+    const page1 = await page1Promise;
+    await page1.getByLabel('Email or phone').click();
+    await page1.getByLabel('Email or phone').fill(foederLoginEmail);
+    await page1.getByRole('button', { name: 'Next' }).click();
+    await page1.getByLabel('Enter your password').click();
+    await page1.getByLabel('Enter your password').fill(foederLoginPassword);
+    await page1.getByRole('button', { name: 'Next' }).click();
+    await page1.getByRole('button', { name: /Doorgaan|Next/ }).click();
+    await expect(page.getByRole('paragraph')).toContainText('Welcome TestAccount');
+});
