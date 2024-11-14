7fba3028c8cbc9b274f6a4fd1141b2071666ac30
Cynthia De Meester <439122@student.fontys.nl>
2024-11-14 11:45:34 +0100

    updated for stealth to work

--- Foeder-Frontend/tests/login.spec.js
+++ Foeder-Frontend/tests/login.spec.js
@@ -1,4 +1,4 @@
-import { test as base, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import dotenv from 'dotenv';
 import {chromium} from 'playwright-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
@@ -7,38 +7,32 @@ dotenv.config({ path: './secrets.env' });
 
 chromium.use(StealthPlugin());
 
-const test = base.extend({
-    browser: async ({}, use) => {
-        const browser = await chromium.launch({
-            headless: true,
-        });
-        await use(browser);
-        await browser.close();
-    },
-});
-
 const foederLoginEmail = process.env.foederLoginEmail;
 const foederLoginPassword = process.env.foederLoginPass;
 
 test.describe("With stealth plugin", () => {
     test.use({ignoreHTTPSErrors: true});
 
-    test('login', async (browser) => {
-        const page = await browser.newPage();
-        await page.goto('https://localhost:5173/');
-        await page.setViewportSize({ width: 1600, height: 900 }); // Adjust to your preference
+    test('login', async () => {
+        chromium.launch({ headless: true }).then(async browser => {
+            const page = await browser.newPage()
 
-        const page1Promise = page.waitForEvent('popup');
-        await page.locator('iframe[title="Knop Inloggen met Google"]').contentFrame().locator('#container').click();
-        const page1 = await page1Promise;
-        await page1.getByLabel('Email or phone').click();
-        await page1.getByLabel('Email or phone').fill(foederLoginEmail);
-        await page1.getByRole('button', { name: 'Next' }).click();
-        await page1.getByLabel('Enter your password').click();
-        await page1.getByLabel('Enter your password').fill(foederLoginPassword);
-        await page1.getByRole('button', { name: 'Next' }).click();
-        await page1.getByRole('button', { name: /Doorgaan|Next/ }).click();
-        await expect(page.getByRole('paragraph')).toContainText('Welcome TestAccount');
+            await page.goto('https://localhost:5173/');
+            await page.setViewportSize({ width: 1600, height: 900 }); // Adjust to your preference
+
+            const page1Promise = page.waitForEvent('popup');
+            await page.locator('iframe[title="Knop Inloggen met Google"]').contentFrame().locator('#container').click();
+            const page1 = await page1Promise;
+            await page1.getByLabel('Email or phone').click();
+            await page1.getByLabel('Email or phone').fill(foederLoginEmail);
+            await page1.getByRole('button', { name: 'Next' }).click();
+            await page1.getByLabel('Enter your password').click();
+            await page1.getByLabel('Enter your password').fill(foederLoginPassword);
+            await page1.getByRole('button', { name: 'Next' }).click();
+            await page1.getByRole('button', { name: /Doorgaan|Next/ }).click();
+            await expect(page.getByRole('paragraph')).toContainText('Welcome TestAccount');
+        })
+               
     });
 })
 
