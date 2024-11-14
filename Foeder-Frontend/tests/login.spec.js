97adefaf6def57e5b5a61d60e502ca6391eff5f3
Cynthia De Meester <439122@student.fontys.nl>
2024-11-14 11:04:31 +0100

    Changed test environment for playwright

--- Foeder-Frontend/tests/login.spec.js
+++ Foeder-Frontend/tests/login.spec.js
@@ -1,20 +1,34 @@
-import { test, expect } from '@playwright/test';
+import { test as base, expect } from '@playwright/test';
 import dotenv from 'dotenv';
+import {chromium} from 'playwright-extra';
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+
 dotenv.config({ path: './secrets.env' });
 
+chromium.use(StealthPlugin());
 
+const test = base.extend({
+    browser: async ({}, use) => {
+        const browser = await chromium.launch({
+            headless: false,
+        });
+        await use(browser);
+        await browser.close();
+    },
+});
 
 const foederLoginEmail = process.env.foederLoginEmail;
 const foederLoginPassword = process.env.foederLoginPass;
-const userAgent =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36";
 
-test.describe("With user agent", () => {
-    test.use({userAgent, ignoreHTTPSErrors: true});
+test.describe("With stealth plugin", () => {
+    test.use({ignoreHTTPSErrors: true});
 
-    test('login', async ({page}) => {
-
+    test('login', async () => {
+        const browser = await chromium.launch({headless: false});
+        const page = await browser.newPage();
         await page.goto('https://localhost:5173/');
+        await page.setViewportSize({ width: 1600, height: 900 }); // Adjust to your preference
+
         const page1Promise = page.waitForEvent('popup');
         await page.locator('iframe[title="Knop Inloggen met Google"]').contentFrame().locator('#container').click();
         const page1 = await page1Promise;
