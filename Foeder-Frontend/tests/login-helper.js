f1019d348b700a08cff47e0ebd5b341ec82ab75d
Cynthia De Meester <439122@student.fontys.nl>
2024-11-27 21:53:52 +0100

    fixed bug with E2E testing

--- Foeder-Frontend/tests/login-helper.js
+++ Foeder-Frontend/tests/login-helper.js
@@ -1,12 +1,9 @@
 export const login = async (page, foederLoginEmail, foederLoginPassword) => {
     await page.goto('https://localhost:5173/');
-    await page.setViewportSize({ width: 1600, height: 900 }); // Adjust to your preference
-
+    await page.waitForTimeout(2000);
     const page1Promise = page.waitForEvent('popup');
-    const iframe = await page.locator('iframe[title="Knop Inloggen met Google"]').contentFrame();
-    await iframe.locator('#container').click();
+    await page.locator('iframe[title="Knop Inloggen met Google"]').contentFrame().getByRole('button').click();
     const page1 = await page1Promise;
-    await page1.waitForLoadState();
     await page1.getByLabel('Email or phone').click();
     await page1.getByLabel('Email or phone').fill(foederLoginEmail);
     await page1.getByRole('button', { name: 'Next' }).click();
--- Foeder-Frontend/tests/login.spec.js
+++ Foeder-Frontend/tests/login.spec.js
@@ -6,7 +6,11 @@ import {login} from "./login-helper.js";
 
 dotenv.config({path: './secrets.env'});
 
+const stealthPlugin = StealthPlugin();
+stealthPlugin.enabledEvasions.delete('iframe.contentWindow');
+stealthPlugin.enabledEvasions.delete('navigator.plugins');
 
+chromium.use(stealthPlugin);
 
 const foederLoginEmail = process.env.foederLoginEmail;
 const foederLoginPassword = process.env.foederLoginPass;
@@ -15,25 +19,25 @@ test.describe("With stealth plugin", () => {
     test.use({ignoreHTTPSErrors: true});
 
     test('login', async () => {
-        chromium.use(StealthPlugin());
-        await chromium.launch({headless: false}).then(async browser => {
+        await chromium.launch({headless: true}).then(async browser => {
+
             const page = await browser.newPage()
 
             await login(page, foederLoginEmail, foederLoginPassword);
 
-            await expect(page.getByRole('link', { name: 'Recipes' })).toBeVisible();
+            await expect(page.getByRole('link', {name: 'Recipes'})).toBeVisible();
             await browser.close();
         })
-
     });
 
     test('add-household', async () => {
-        await chromium.launch({headless: false}).then(async browser => {
+        await chromium.launch({headless: true}).then(async browser => {
+
             const page = await browser.newPage()
 
             await login(page, foederLoginEmail, foederLoginPassword);
 
-            await page.waitForTimeout(3000);
+            await page.waitForTimeout(2000);
             await page.getByRole('link', {name: 'Household'}).click();
             await page.getByRole('link', {name: 'Create a household'}).click();
             await page.getByPlaceholder('Household name').click();
@@ -43,10 +47,11 @@ test.describe("With stealth plugin", () => {
             await browser.close()
 
         })
-    })
+    });
 
     test('view-recipes', async () => {
-        await chromium.launch({headless: false}).then(async browser => {
+
+        await chromium.launch({headless: true}).then(async browser => {
             const page = await browser.newPage()
 
             await login(page, foederLoginEmail, foederLoginPassword);
