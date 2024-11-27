11a98eef6a5cab428bc6dcf1c3e2964ff35c8700
Cynthia De Meester <439122@student.fontys.nl>
2024-11-27 20:00:24 +0100

    Test pipeline again

--- Foeder-Frontend/tests/login-helper.js
+++ Foeder-Frontend/tests/login-helper.js
@@ -3,8 +3,10 @@ export const login = async (page, foederLoginEmail, foederLoginPassword) => {
     await page.setViewportSize({ width: 1600, height: 900 }); // Adjust to your preference
 
     const page1Promise = page.waitForEvent('popup');
-    await page.locator('iframe[title="Knop Inloggen met Google"]').contentFrame().locator('#container').click();
+    const iframe = await page.locator('iframe[title="Knop Inloggen met Google"]').contentFrame();
+    await iframe.locator('#container').click();
     const page1 = await page1Promise;
+    await page1.waitForLoadState();
     await page1.getByLabel('Email or phone').click();
     await page1.getByLabel('Email or phone').fill(foederLoginEmail);
     await page1.getByRole('button', { name: 'Next' }).click();
