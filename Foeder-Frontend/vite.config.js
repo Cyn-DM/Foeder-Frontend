22340ac7dc85c7465f9787e7f16d8671eb8787dc
Cynthia De Meester <439122@student.fontys.nl>
2024-12-14 16:23:40 +0100

    changed viteconfig to fit docker env variable

--- Foeder-Frontend/vite.config.js
+++ Foeder-Frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import mkcert from 'vite-plugin-mkcert'
 
-const isDocker = process.env.IN_DOCKER === 'true'
+const isDocker = process.env.VITE_DOCKER === 'true'
 
 // https://vitejs.dev/config/
 export default defineConfig({
