76fc5d1585ab5c06b4db45aea238e37c0138cc6b
Cynthia De Meester <439122@student.fontys.nl>
2024-12-28 11:26:14 +0100

    Added functionality to view one recipe

--- Foeder-Frontend/src/Components/Recipe.jsx
+++ Foeder-Frontend/src/Components/Recipe.jsx
@@ -1,45 +1,100 @@
 import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {UseAuth} from "../Authentication/AuthProvider.jsx";
 
 export function Recipe() {
     const {id} = useParams();
+    const {axiosInstance} = UseAuth();
+    const [recipe, setRecipe] = useState(null);
     
     
-    return (<>
-            <div className="min-h-screen mx-auto w-[80%] overflow-visible mb-8 pb-16">
-                <div className="flex flex-col mt-10 px-6 md:px-20 xl:px-72 inter-mainFont overflow-y-auto">
-                    <div className="flex flex-col">
-                        <div className="w-full">
-                            <div className="flex justify-between flex-row flex-wrap-reverse gap-8 mb-8">
-                                <div className="flex flex-col">
-                                    <div className="inter-mainFont font-bold text-5xl mb-5">
-                                        Title
-                                    </div>
-                                    <div className="inter-mainFont text-xl">
-                                        Description
-                                    </div>
-                                </div>
-                                <div className="flex flex-col">
-                                    <div className="image-container recipe-image aspect-square mb-4">
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="flex flex-col w-full bg-white rounded-lg mb-8 p-8">
-                                <div className="inter-mainFont font-bold text-lg">
-                                    Ingredients
-                                </div>
-                            </div>
-                            <div className="flex flex-col w-full bg-white rounded-lg p-8 mb-8">
-                                
-                            </div>
-                            <div>
-                            </div>
-                        </div>
+    useEffect(() => {
+        axiosInstance.get(`/Recipe/GetRecipe?recipeId=${id}`)
+            .then((response) => {setRecipe(response.data); console.log(response.data);})
+            .catch((error) => {
+                console.log(error);
+            });
+    }  , []
+    );
+
+
+
+    if (recipe != null){
+        return (
+            <div className="recipe-container mb-8">
+                <div className="recipe-title-desc-container recipe-part-card">
+                    <div className="recipe-title mb-6">
+                        {recipe.title}
+                    </div>
+                    <div className="recipe-description">
+                        {recipe.description}
+                    </div>
+                </div>
+                <div className="recipe-image-container recipe-part-card">
+                    <RecipeImage />
+                </div>
+                <div className="ingredients-container flex flex-col basis-20 recipe-part-card">
+                    <div className="ingredient-header mb-6">
+                        Ingredients
+                    </div>
+
+                    <div className="overflow-x-auto bg-base-100 ">
+
+                        <table className="min-w-full text-left text-sm whitespace-nowrap">
+
+                            <thead className="uppercase tracking-wider border-b-2 ">
+                            <tr>
+                                <th scope="col" className="px-6 py-4">
+                                    Ingredient Name
+                                </th>
+                                <th scope="col" className="px-6 py-4">
+                                    Amount
+                                </th>
+                            </tr>
+                            </thead>
+
+                            <tbody>
+                            {recipe.ingredients.map((ingredient) => (
+                                    <tr key={ingredient} className="border-b hover:bg-base-200 ">
+                                        <th scope="row" className="px-6 py-4">
+                                            {ingredient.name}
+                                        </th>
+                                        <td className="px-6 py-4">{ingredient.amount}</td>
+                                    </tr>
+                                )
+                            )}
+
+
+                            </tbody>
+
+                        </table>
+
                     </div>
 
                 </div>
-
+                <div className="steps-container recipe-part-card">
+                    <div className="step-header mb-6">
+                        Steps
+                    </div>
+                    <ol>
+                        {recipe.steps.map((step, index) => (
+                            <li key={index}>{step}</li>
+                        ))}
+                    </ol>
+                </div>
             </div>
-        </>
 
+        )
+    } 
+    
+    return (
+        <></>
     )
+    
+}
+
+function RecipeImage({image}) {
+    if (image === undefined) {
+        return <img src={'/img/No-Image-Placeholder.png'} alt="Not found"/>
+    }
 }
